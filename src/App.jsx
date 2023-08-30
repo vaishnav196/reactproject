f8ffd931a72e9b66fc03bbab713a330a2d783e60
vaishnav196 <vaish00721@gmail.com>
2023-08-30 16:24:42 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
+import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/Home'
 import Footer from './components/Footer';
-import History from './components/History';
-import Core from './components/Core';
-import IT from './components/IT';
-import Distribute from './components/Distribute';
-import {   BrowserRouter as Routes, Route, Router } from "react-router-dom";
+// import History from './components/History';
+// import Core from './components/Core';
+// import IT from './components/IT';
+// import Distribute from './components/Distribute';
 
-import Leader from "./components/Leader";
+
+// import Leader from "./components/Leader";
 
 
 
 function App() {
   return (
     <div>
-
-
-<Router>
-<Navbar/>
-
-  <Routes>
-     <Route  path='/' element={< Home />}></Route>
-     <Route  path='History/' element={< History/>}></Route>
-     <Route path='Core/' element={< Core/>}></Route>
-     <Route path='Distribute/' element={<Distribute/>}></Route>
-     <Route  path='IT/' element={< IT/>}></Route>
-     <Route  path='History/' element={< History/>}></Route>
-     <Route  path='Leader/' element={< Leader/>}></Route>
-    
-  </Routes>
+  <Navbar/>
+  <Home/>
   <Footer/>
-</Router>
-
-
-
-
-
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import './Navbar.css';
 import {BsFillBagFill} from 'react-icons/bs';
-import { Link } from "react-router-dom";
+
 
 
 
@@ -9,14 +9,14 @@ function Navbar() {
 
   return (
   
-    <div ClassNameName="Navbar">
+    <div ClassName="Navbar">
   <header>
-    <div ClassNameName="container-lg">
-      <div ClassNameName="row">
-        <div ClassNameName="col">
-        <nav ClassNameName="navbar  navbar-expand-lg  justify-content-center ">
+    <div ClassName="container-lg">
+      <div ClassName="row">
+        <div ClassName="col">
+        <nav ClassName="navbar  navbar-expand-lg  justify-content-center ">
   <div ClassName="container-lg">
-    <div ClassName="navbar-brand" to="#"><img src="img/logo.png" alt="" ClassNameName="img-fluid" /></div>
+    <div ClassName="navbar-brand" href="#"><img src="img/logo.png" alt="j" ClassName="img-fluid" /></div>
     <div ClassName=" navbar-toggle d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span ClassName="fas fa-bars " ></span>
     <span ClassName="fas fa-times d-none"></span>
@@ -25,67 +25,67 @@ function Navbar() {
     <div ClassName="collapse navbar-collapse justify-content-between " id="navbarSupportedContent">
       <ul ClassName="navbar-nav m-auto mb-2 mb-lg-0">
         <li ClassName="nav-item">
-          <Link ClassName="nav-link px-3 " aria-current="page" to="/">Home</Link>
+          <a ClassName="nav-link px-3 " aria-current="page" href="/">Home</a>
         </li>
         <li ClassName="nav-item dropdown">
-          <a ClassName="nav-link px-3 " to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a ClassName="nav-link px-3 " href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
            About<i ClassName="fa-solid fa-chevron-down"></i>
           </a>
           <ul ClassName="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><Link ClassName="dropdown-item" to="/History">History</Link></li>
+            <li><a ClassName="dropdown-item" href="History">History</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="#">Leadership</Link></li>
+            <li><a ClassName="dropdown-item" href="">Leadership</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="/Core">Core values</Link></li>
+            <li><a ClassName="dropdown-item" href="Core">Core values</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="/Distribute">Distributation</Link></li>
+            <li><a ClassName="dropdown-item" href="Distribute">Distributation</a></li>
     
           </ul>
         </li>
         <li ClassName="nav-item dropdown">
-          <Link ClassName="nav-link px-3" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a ClassName="nav-link px-3" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
            Products<i ClassName="fa-solid fa-chevron-down" ></i>
-          </Link>
+          </a>
           <ul ClassName="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><Link ClassName="dropdown-item" to="#">Personal care</Link></li>
+            <li><a ClassName="dropdown-item" href="#">Personal care</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="#">Bathroom Care</Link></li>
+            <li><a ClassName="dropdown-item" href="#">Bathroom Care</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="#">Floor & Surafce Care</Link></li>
+            <li><a ClassName="dropdown-item" href="#">Floor & Surafce Care</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="#">General Care</Link></li>
+            <li><a ClassName="dropdown-item" href="#">General Care</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="#">Kitchen Care</Link></li>
+            <li><a ClassName="dropdown-item" href="#">Kitchen Care</a></li>
           </ul>
         </li>
         <li ClassName="nav-item">
-          <Link ClassName="nav-link px-3" to="#" tabindex="-1" >Innovation & Technology</Link>
+          <a ClassName="nav-link px-3" href="#" tabindex="-1" >Innovation & Technology</a>
         </li>
         <li ClassName="nav-item dropdown">
-          <Link ClassName="nav-link px-3 " to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a ClassName="nav-link px-3 " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Media<i ClassName="fa-solid fa-chevron-down" ></i>
-          </Link>
+          </a>
           <ul ClassName="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><Link ClassName="dropdown-item" to="/">Facebook</Link></li>
+            <li><a ClassName="dropdown-item" href="/">Facebook</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="#">Instagram</Link></li>
+            <li><a ClassName="dropdown-item" href="#">Instagram</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="/">Linkdin</Link></li>
+            <li><a ClassName="dropdown-item" href="/">Linkdin</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="/">Twitter</Link></li>
+            <li><a ClassName="dropdown-item" href="/">Twitter</a></li>
             <li><hr ClassName="dropdown-divider"/></li>
-            <li><Link ClassName="dropdown-item" to="/">Youtube</Link></li>
+            <li><a ClassName="dropdown-item" href="/">Youtube</a></li>
           </ul>
         </li>
       </ul>
      
-       <div ClassNameName="button">
+       <div ClassName="button">
 
       <button ClassName="btn btn-outline-green " type="submit">Contact Us</button>
        </div>
       
     </div>
-    <div ClassNameName="right-nav">
+    <div ClassName="right-nav">
 
     <a ><BsFillBagFill fontSize={"35px"}  color='#1dbfc1'/></a>
     </div>

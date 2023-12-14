7c02d01a65dcae1e3ccca88a015fbc6fe03a8dd1
vaishnav196 <vaish00721@gmail.com>
2023-12-14 15:20:46 +0530

    productcares

--- src/components/Bathroomcare.jsx
+++ src/components/Bathroomcare.jsx
@@ -3,7 +3,80 @@ import React from 'react'
 export default function Bathroomcare() {
   return (
     <div>
-      
+        <div style={{ backgroundColor: "#effafa" }}>
+      <div className="container-lg">
+        <div className="row">
+          <div className="all mt-5  d-flex ">
+            <h6>Showing all 3 Products</h6>
+            <select class="form-select w-25 ms-auto border-0 " aria-label="Default select example">
+              <option selected>Default Sorting</option>
+              <option value="1">Sort by Popularity</option>
+              <option value="2">Sort by average rating</option>
+              <option value="3">Sort by latest</option>
+              <option value="4">Sort by price:low to high</option>
+              <option value="5">Sort by price: high to low</option>
+            </select>
+          </div>
+          <div className="col-md-3 py-3">
+            <div className="card  w-75 border-0" style={{ minWidth: "20rem" }}>
+              <img
+                src="img/bottle1.jpeg"
+                class=" img-fluid mx-auto "
+                alt="..."
+              />
+              <div className="card-body">
+                <h6 className="">Personal care</h6>
+                <h6 className="text-success">
+                  Bathroom Care Grabbit First Hand Sanitizer 5 Litres
+                </h6>
+                <h6 className="align-end text-black">Rs 999.000</h6>
+                <a href="#" class="btn btn-success">
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 py-3">
+            <div className="card  w-75 border-0" style={{ minWidth: "20rem" }}>
+              <img
+                src="img/bottle1.jpeg"
+                class=" img-fluid mx-auto "
+                alt="..."
+              />
+              <div className="card-body">
+                <h6 className="">Personal care</h6>
+                <h6 className="text-success">
+                  Personal Care Grabbit First Hand Sanitizer 5 Litres
+                </h6>
+                <h6 className="align-end text-black">Rs 999.000</h6>
+                <a href="#" class="btn btn-success">
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 py-3">
+            <div className="card  w-75 border-0" style={{ minWidth: "20rem" }}>
+              <img
+                src="img/bottle1.jpeg"
+                class=" img-fluid mx-auto "
+                alt="..."
+              />
+              <div className="card-body">
+                <h6 className="">Personal care</h6>
+                <h6 className="text-success">
+                  Personal Care Grabbit First Hand Sanitizer 5 Litres
+                </h6>
+                <h6 className="align-end text-black">Rs 999.000</h6>
+                <a href="#" class="btn btn-success">
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
--- src/components/Products/Personalcare.jsx
+++ src/components/Products/Personalcare.jsx
@@ -16,7 +16,26 @@ export default function Personalcare() {
               <option value="5">Sort by price: high to low</option>
             </select>
           </div>
-          <div className="col-md-4 py-3">
+          <div className="col-md-3 py-3">
+            <div className="card  w-75 border-0" style={{ minWidth: "20rem" }}>
+              <img
+                src="img/bottle1.jpeg"
+                class=" img-fluid mx-auto "
+                alt="..."
+              />
+              <div className="card-body">
+                <h6 className="">Personal care</h6>
+                <h6 className="text-success">
+                  Personal Care Grabbit First Hand Sanitizer 5 Litres
+                </h6>
+                <h6 className="align-end text-black">Rs 999.000</h6>
+                <a href="#" class="btn btn-success">
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 py-3">
             <div className="card  w-75 border-0" style={{ minWidth: "20rem" }}>
               <img
                 src="img/bottle1.jpeg"

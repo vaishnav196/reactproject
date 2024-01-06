fd004c1dd660a04569be06f5d4e87a86be3ab5fc
vaishnav196 <vaish00721@gmail.com>
2024-01-06 13:58:12 +0530

    .

--- src/components/Productdetails.jsx
+++ src/components/Productdetails.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
 
 export default function Productdetails() {
   return (
-    <div>
-      
+    <div className="" style={{ backgroundColor: "#effafa" }}>
+      <div className="container">
+        <div className="row ">
+          <div className="col-md-5 ">
+            <img
+              src="/img/Hand-wash_Grabbit-first-01.jpg"
+              alt=""
+              className="img-fluid"
+            />
+            <div className="row mt-3">
+              <div className="col-md-3">
+                <img
+                  src="/img/Hand-wash_Grabbit-first-01.jpg"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
+              <div className="col-md-3">
+                <img
+                  src="/img/Hand-wash_Grabbit-first-02-100x100.jpg"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-7  ">
+            <h1>Grabbit First Hand Wash 5 Litres</h1>
+            <p>
+              Grabbit First Hand Wash is an effective solution that is hard on
+              germs, soft on hands and gives a pleasant fragrance.
+            </p>
+
+            <p>
+              Grabbit First Hand Wash is crafted with care and features a
+              carefully balanced formula to protect from germs and maintain skin
+              pH. It contains excellent antioxidant
+            </p>
+
+            <p>
+              Vitamin E, which provides long-lasting moisturizing to the skin.
+              It also provides a pleasant fragrance while remaining gentle on
+              your skin.
+            </p>
+
+            <p>
+              Experience the perfect balance between cleanliness and care with
+              Grabbit First HandWash.
+            </p>
+            
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
